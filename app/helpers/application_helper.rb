7086d49b4eb4c6cfe4ebbf84e092565e512a99b3
dmichaux <david.michaux@pennmedicine.upenn.edu>
2024-03-01 16:26:58 -0500

    raven_image_link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def raven_image_link
+    poem = Writing.find_by(name: 'The Raven', author_last: 'Poe')
+    link_to(
+        image_tag('raven.png', alt: 'raven', class: 'img-fluid m-auto p-2 ps-3'),
+        collection_writing_path(poem.collection, poem)
+    )
+  end
 end
