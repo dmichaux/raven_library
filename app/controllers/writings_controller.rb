a2280a8fe96227c59a46d206067cec5833b25155
dmichaux <david.michaux@pennmedicine.upenn.edu>
2024-03-01 14:39:35 -0500

    some fixes

--- app/controllers/writings_controller.rb
+++ app/controllers/writings_controller.rb
@@ -1,24 +1,27 @@
 class WritingsController < ApplicationController
   # before_action :authenticate_user!, only: %i[new create]
+  before_action :find_collection, only: %i[show new create]
 
   def show
     @writing = Writing.find params[:id]
-    @collection = @writing.collection
   end
 
   def new
-    @collection = Collection.find params[:collection_id]
     @writing = @collection.writings.build
   end
 
   def create
     writing = Writing.new(writing_params)
-    redirect_to writing if writing.save
+    redirect_to collection_writing_path(@collection, writing) if writing.save
   end
 
   private
 
   def writing_params
     params.require(:writing).permit(:collection_id, :author_first, :author_middle, :author_last, :name, :content)
+  end
+
+  def find_collection
+    @collection = Collection.find params[:collection_id]
   end
 end
