0ca47074d485e987ce08ef2427271e8bd3dba3b2
dmichaux <david.michaux@pennmedicine.upenn.edu>
2024-03-30 21:14:10 -0400

    Add published_at and archived_at

--- db/migrate/20240330203231_create_genres.rb
+++ db/migrate/20240330203231_create_genres.rb
@@ -2,6 +2,8 @@ class CreateGenres < ActiveRecord::Migration[7.1]
   def change
     create_table :genres do |t|
       t.string :name, null: false
+      t.datetime :published_at
+      t.datetime :archived_at
       t.timestamps
     end
   end
