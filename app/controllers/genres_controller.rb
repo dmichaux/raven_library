5efaee13995e45505577d14f7327028e880ba6b3
dmichaux <david.michaux@pennmedicine.upenn.edu>
2024-03-30 21:14:19 -0400

    genre controller

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,41 @@
+class GenresController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create update]
+
+  def index
+    @genres = Genre.published.alpha_order unless user_signed_in?
+    @genres = Genre.alpha_order if user_signed_in?
+  end
+
+  def show
+    @genre = Genre.includes(:collections).find params[:id]
+    @collections = @genre.published_collections.alpha_order unless user_signed_in?
+    @collections = @genre.collections.alpha_order if user_signed_in?
+  end
+  
+  def new
+    @genre = Genre.new
+  end
+  
+  def create
+    @genre = Genre.new(genre_params)
+    redirect_to @genre if @genre.save
+  end
+  
+  # So far, only for (un)publishing
+  # TODO: should be calling @genre.publish, instead
+  def update
+    @genre = Genre.find params[:id]
+    @genre.update(update_genre_params)
+    redirect_to @genre
+  end
+
+  private
+
+  def create_genre_params
+    params.require(:genre).permit(:name)
+  end
+
+  def update_genre_params
+    params.require(:genre).permit(:published_at)
+  end
+end
