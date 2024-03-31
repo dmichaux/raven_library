8e3123ded4be13bbd39f8a2085f56b13577dd7ff
dmichaux <david.michaux@pennmedicine.upenn.edu>
2024-03-30 21:13:35 -0400

    Add metadata methods

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,39 @@
 class Genre < ApplicationRecord
-  has_many :collections
+  has_many :collections,
+           dependent: :restrict_with_exception
+  has_many :published_collections,
+           -> { where('collections.published_at IS NOT NULL') },
+           class_name: 'Collection',
+           dependent: :restrict_with_exception
+
+  scope :alpha_order, -> { order(:name) }
+  scope :published, -> { where.not(published_at: nil) }
+
+  def published?
+    published_at.present?    
+  end
+
+  def publish
+    return if archived? || published?
+
+    update(published_at: Time.zone.now)
+  end
+
+  def unpublish
+    return unless published?
+
+    update(published_at: nil)
+  end
+
+  def archived?
+    archived_at.present?    
+  end
+
+  def archive
+    update(archived_at: Time.zone.current)
+  end
+  
+  def de_archive
+    update(archived_at: nil)
+  end
 end
