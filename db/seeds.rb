7ec177e7eac820f488bebd1159e731a9ee43c230
dmichaux <david.michaux@pennmedicine.upenn.edu>
2024-04-01 09:12:59 -0400

    publish seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
-p = Genre.find_or_create_by(name: 'Poetry')
-Collection.find_or_create_by(name: 'Poe', genre: p)
-Collection.find_or_create_by(name: 'Henley', genre: p)
-Collection.find_or_create_by(name: 'Thomas', genre: p)
+p = Genre.find_or_create_by(name: 'Poetry', published_at: Time.current)
+Collection.find_or_create_by(name: 'Poe', genre: p, published_at: Time.current)
+Collection.find_or_create_by(name: 'Henley', genre: p, published_at: Time.current)
+Collection.find_or_create_by(name: 'Thomas', genre: p, published_at: Time.current)
 
 Dir[Rails.root.join('db', 'seeds', '*.rb').to_s].each { |file| require file }
