ed63349766cfe6898f6f372ab5dfd7e380f2b486
dmichaux <david.michaux@pennmedicine.upenn.edu>
2024-04-04 13:17:31 -0400

    don't set ssh opts

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,4 +38,4 @@ set :keep_releases, 5
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
-set :ssh_options, { forward_agent: true, user: "deploy", auth_methods: ['publickey'], keys: %w(~/.ssh/id_rsa) }
+# set :ssh_options, { forward_agent: true, user: "deploy", auth_methods: ['publickey'], keys: %w(~/.ssh/id_rsa) }
